e799dc0ec879c21c7e339acf46c12d28f69e4e8e
alexisCode50 <skrillex.ja14@gmail.com>
2021-11-09 22:35:45 -0600

    agregamos rewrites para apis externas

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://newsapi.org/:path*', // Matched parameters can be used in the destination
+      },
+    ]
+  },
 }
