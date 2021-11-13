7611e66a6fd183f390c975532c0860898fa4a9a7
alexisCode50 <skrillex.ja14@gmail.com>
2021-11-12 21:35:30 -0600

    ajustes en el endpoint de busqueda

--- pages/api/search.js
+++ pages/api/search.js
@@ -4,13 +4,7 @@ const handler = async (req, res) => {
 
   const { keyword } = req.query;
 
-  const today  = new Date();
-
-  const customDate = today.toLocaleDateString("en-US")
-
-  const currentDateFormat = customDate.split('/').reverse().join('-')
-
-  const { data } = await httpClient(`/everything?q=${keyword}&from=${currentDateFormat}&ortBy=publishedAt`);
+  const { data } = await httpClient(`/everything?q=${keyword}`);
 
   res.status(200).json({ articles: data });
 }
